540f56b937ae294755e83b643717f0d729b64e78
Keith Smiley <keithbsmiley@gmail.com>
2023-06-30 10:36:27 -0700

    Reorder help by most useful

--- src/cli.rs
+++ src/cli.rs
@@ -4,31 +4,31 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// Path of the binary to print dylibs for
+    /// Path of the binary to print dynamic dependencies for
     #[arg(value_name = "BINARY")]
     pub binary: PathBuf,
 
-    /// Exclude all duplicate dylib names, reduces output but excludes first level
-    /// dependencies as well
-    #[arg(short, long, default_value_t = false)]
-    pub exclude_all_duplicates: bool,
+    /// The maximum depth of libraries to print. Reduce to reduce output
+    #[arg(short, long, default_value_t = 9999)]
+    pub depth: usize,
 
     /// Exclude dylibs that start with these prefixes
     #[arg(short = 'p', long, value_name = "PREFIX")]
     pub ignore_prefix: Vec<String>,
-
-    /// Path to the shared cache, if not provided try to discover it.
-    #[arg(short, long)]
-    pub shared_cache_path: Option<PathBuf>,
 
     /// Path to the a device or simulators root runtime directory containing the dylibs for
     /// the OS, if not provided try to discover it.
     #[arg(short, long)]
     pub runtime_root: Option<PathBuf>,
 
-    /// The maximum depth of libraries to print. Reduce to reduce output
-    #[arg(short, long, default_value_t = 9999)]
-    pub depth: usize,
+    /// Path to the shared cache, if not provided try to discover it.
+    #[arg(short, long)]
+    pub shared_cache_path: Option<PathBuf>,
+
+    /// Exclude all duplicate dylib names, reduces output but excludes first level
+    /// dependencies as well
+    #[arg(short, long, default_value_t = false)]
+    pub exclude_all_duplicates: bool,
 
     /// Print verbose output for debugging dylibtree itself
     #[arg(long, default_value_t = false, hide = true)]
